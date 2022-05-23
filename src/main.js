6e9ac3a0d1fcd86f570daf149ebdc7af9602b853
Mark Landeryou <samurai96@hotmail.com>
2022-05-23 15:46:28 -0400

    made some changes

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import './styles.css';
+import './styles.css'
 
 // document.querySelector('#app').innerHTML = `
 //   <h1>Hello Vite!</h1>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
+/* eslint-disable no-undef */
 module.exports = {
   content: ['./index.html'],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
