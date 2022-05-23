c46705e66c438131c74779998b2c4978effdfe78
Mark Landeryou <samurai96@hotmail.com>
2022-05-23 16:46:58 -0400

    refactor: removed prettier for tailwindconfig and main js file

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import './styles.css'
 
 // document.querySelector('#app').innerHTML = `
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 /* eslint-disable no-undef */
 module.exports = {
   content: ['./index.html'],
