108f8c22335dad51758a5eadc6bfb494ea2d0a2f
Mark Landeryou <samurai96@hotmail.com>
2022-06-11 06:40:27 -0400

    refactor: trying to refactor code for scroll to top functionality

--- src/main.js
+++ src/main.js
@@ -1,7 +1,32 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 /* eslint-disable prettier/prettier */
 import './styles.css'
+import './backToTop.js'
 
 // document.querySelector('#app').innerHTML = `
 //   <h1>Hello Vite!</h1>
 //   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 // `;
+
+//Get the button:
+mybutton = document.getElementById('myBtn')
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction()
+}
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = 'block'
+  } else {
+    mybutton.style.display = 'none'
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0 // For Safari
+  document.documentElement.scrollTop = 0 // For Chrome, Firefox, IE and Opera
+}
